22d5836b0cf8650ba97e18f37682e6835329d0cc
paper_man <sonnoleggero5@gmail.com>
2024-11-19 08:29:38 +0100

    prova

--- src/game/Map.java
+++ src/game/Map.java
@@ -4,7 +4,7 @@ import map_creator.Peg;
 
 import java.util.ArrayList;
 
-public class Map {
+public class Map { //sta classe non serve
     ArrayList <Peg> listaPegs;
     public Map(ArrayList<Peg> arraylitPegs){
         this.listaPegs=arraylitPegs;
